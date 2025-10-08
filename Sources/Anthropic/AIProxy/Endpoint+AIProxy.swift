af1277f9f0ab2d68795f31781e0f9f593fd0dad8
Kyle Macomber <kylemacomber@gmail.com>
2025-10-08 13:01:19 -0700

    Sorts keys when enocding httpBody

--- Sources/Anthropic/AIProxy/Endpoint+AIProxy.swift
+++ Sources/Anthropic/AIProxy/Endpoint+AIProxy.swift
@@ -67,6 +67,7 @@ extension Endpoint {
       if let params {
          let encoder = JSONEncoder()
          encoder.keyEncodingStrategy = .convertToSnakeCase
+         encoder.outputFormatting = .sortedKeys
          request.httpBody = try encoder.encode(params)
       }
       return request
